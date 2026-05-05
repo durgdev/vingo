ef03860211f660bbd58d47acea9a14c72f333117
durgdev <durgeshyadav.2029@gmail.com>
2026-05-05 17:18:26 +0530

    fix vercel  backend

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,4 @@
-// import express from "express"
-// import dotenv from "dotenv"
-// dotenv.config()
-// import connectDb from "./config/db.js"
-// import cookieParser from "cookie-parser"
-// import authrouter from "./routes/auth.routes.js"
-// import cors from "cors"
-// import userrouter from "./routes/user.routes.js"
-// import shoprouter from "./routes/shop.routes.js"
-// import itemrouter from "./routes/item.routes.js"
-// const app=express()
-// const port=process.env.PORT||5000
-// app.use(cors({
-//     origin: "https://vingo-frontend-dun.vercel.app",
-//     credentials: true
-// }))
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use("/api/auth",authrouter)
-// app.use("/api/user",userrouter)
-// app.use("/api/shop",shoprouter)
-// app.use("/api/item",itemrouter)
-// app.listen(port,()=>{
 
-//     connectDb()
-   
-//     console.log(`server start at ${port}`);
-    
-// })
-
-
-// export default app  
 
 // import express from "express"
 // import dotenv from "dotenv"
@@ -45,7 +14,6 @@
 // const app = express()
 // const port = process.env.PORT || 5000
 
-// app.options("*", cors())
 // app.use(cors({
 //   origin: [
 //     "https://vingo-frontend-dun.vercel.app",
@@ -70,6 +38,7 @@
 
 // export default app
 
+
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
@@ -82,7 +51,6 @@ import shoprouter from "./routes/shop.routes.js"
 import itemrouter from "./routes/item.routes.js"
 
 const app = express()
-const port = process.env.PORT || 5000
 
 app.use(cors({
   origin: [
@@ -101,9 +69,6 @@ app.use("/api/user", userrouter)
 app.use("/api/shop", shoprouter)
 app.use("/api/item", itemrouter)
 
-app.listen(port, () => {
-  connectDb()
-  console.log(`server start at ${port}`)
-})
+connectDb()
 
 export default app
